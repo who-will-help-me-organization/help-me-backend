80abc03e0b5bff784169cd444322b56c76822142
SILVAWesley <wesley.silva@ccc.ufcg.edu.br>
2018-05-25 17:33:19 -0300

    Setup project

---
+++ src/main/java/org/helpme/HelpMeBackendApplication.java
@@ -0,0 +1,12 @@
+package org.helpme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpMeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpMeBackendApplication.class, args);
+	}
+}
